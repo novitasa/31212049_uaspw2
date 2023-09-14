1991acd8547a75c1774b538a54bd460d6e9b672a
novitasa <novitasaridian799@gmail.com>
2023-09-14 10:35:30 +0700

    commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\CastController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello', function () {
+    echo "<h1>Hello laravel</h1>";
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/forminput', [PagesController::class,'FormInput']);
+Route::post('/welcome', [PagesController::class,'Welcome']);
+Route::get('/datatable', function () {
+    return view('datatable.index');
+});
+// CRUD CAST
+Route::get('/cast', [CastController::class,'index']);
+Route::get('/cast/create', [CastController::class,'create']);
+Route::post('/cast', [CastController::class,'store']);
+
+Route::get('/cast/{cast_id}/edit', [CastController::class,'edit']);
+Route::put('/cast/{cast_id}', [CastController::class,'update']);
+Route::delete('/cast/{cast_id}', [CastController::class,'destroy']);
+
